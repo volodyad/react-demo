9249d3d7cd09841218748af8468671d99be3e2e5
Volodymyr Derecha <volodymyrderecha@gmail.com>
2025-03-13 11:13:02 +0200

    fix debounce on search

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,21 +1,23 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect, useState, useCallback } from "react";
 import styles from "./SearchBar.module.scss";
-import { throttle } from "lodash";
+import { debounce } from "lodash";
 export interface SearchBarProps {
   onSearch: (query: string) => void;
 }
 export const SearchBar: FC<SearchBarProps> = ({ onSearch }: SearchBarProps) => {
   const [query, setQuery] = useState("");
-  const handleSearch = throttle((query: string) => {
-    onSearch(query);
-  }, 500);
+
+  const onSearchDebounced = useCallback(debounce(onSearch, 500), [onSearch]);
   useEffect(() => {
-    handleSearch(query);
-  }, [query, handleSearch]);
+    onSearchDebounced(query);
+    return () => {
+      onSearchDebounced.cancel();
+    };
+  }, [query, onSearchDebounced]);
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
   };
-
   return (
     <div className={styles.searchBar}>
       <span role="img" aria-label="search" className={styles.icon}>
